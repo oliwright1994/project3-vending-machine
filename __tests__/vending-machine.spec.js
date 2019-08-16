447da46105d97f42ec70b157bf923c443a669fb0
Oli Wright <oli.wright@blueyonder.co.uk>
2019-08-16 11:24:26 -0700

    add total change, add dispense method and tests

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,9 +1,42 @@
 const VendingMachine = require("../src/vending-machine");
+const inventory = require("../data/inventory.json");
+const change = require("../data/change.json");
+const vm = new VendingMachine(inventory);
 
 describe("Vending Machine", () => {
-  describe("when i put in £2.50 and A3", () => {
+  describe("when i put in £2.50 and A3 (correct payment and stock)", () => {
     it("should return me the item from A3", () => {
-      expect(VendingMachine.dispense("2.50", "A3")).toEqual("Fanta");
+      expect(vm.dispense(2.5, "A3")).toEqual({
+        item: "Sprite",
+        change: 1.0
+      });
+    });
+  });
+
+  describe("when i put in B2 and no money (no payment)", () => {
+    it("should throw an error of please insert payment", () => {
+      expect(() => vm.dispense(undefined, "B2")).toThrow(
+        "Please insert payment"
+      );
+    });
+  });
+  describe("when i put in 80p and B2 (not enough money)", () => {
+    it("should throw an error of not enough money", () => {
+      expect(() => vm.dispense(0.8, "B2")).toThrow(
+        "Not enough credit, please insert £0.70"
+      );
+    });
+  });
+  describe("when i put in £1.5 and B3 (correct payment, item out of stock)", () => {
+    it("should throw an error of item out of stock", () => {
+      expect(() => vm.dispense(1.5, "B3")).toThrow("Item out of stock");
+    });
+  });
+  describe("when i put in £50 and c2 (change is greater than change in machine)", () => {
+    it("should throw an error of not enough change", () => {
+      expect(() => vm.dispense(5000, "C2")).toThrow(
+        "Not enough change. Please use exact change"
+      );
     });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,17 +2,45 @@ let inventory = require("../data/inventory.json");
 let change = require("../data/change.json");
 
 class VendingMachine {
-  constructor(inventory, change) {
+  constructor(inventory) {
     this.inventory = inventory;
-    this.change = change;
+    this.change = { total: 1000 };
   }
 
-  dispense(payment, item) {
-    if (item.price > payment) {
-      throw "Please insert " + (item.price - payment);
-    } else {
-      return true;
+  dispense(payment, selection) {
+    const item = this.inventory[selection];
+    if (
+      this.checkPayment(payment, item.price) &&
+      this.checkStock(item) &&
+      this.calculateChange(payment, item.price)
+    ) {
+      return {
+        item: item.name,
+        change: payment - item.price
+      };
     }
+  }
+  checkPayment(payment, price) {
+    if (typeof payment === "undefined" || payment === 0) {
+      throw Error("Please insert payment");
+    } else if (price > payment) {
+      throw Error(
+        "Not enough credit, please insert £" + (price - payment).toFixed(2)
+      );
+    }
+    return true;
+  }
+  checkStock(item) {
+    if (item.quantity === 0) {
+      throw Error("Item out of stock");
+    }
+    return true;
+  }
+  calculateChange(payment, price) {
+    if (payment - price > this.change.total) {
+      throw Error("Not enough change. Please use exact change");
+    }
+    return true;
   }
 }
 
